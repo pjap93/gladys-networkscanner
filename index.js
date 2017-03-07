616a6e2816786996fba6b1c1a728a151310cd7b5
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-03-07 15:18:13 +0100

    scan network frequently

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 module.exports = function(sails) {
 
     const scan = require('./lib/scan.js');
+    const init = require('./lib/init.js');
+
+    gladys.on('ready', function(){
+        scan();
+        init();
+    });
     
     return {
         scan
